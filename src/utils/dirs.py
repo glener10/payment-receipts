3deb5282ed87ed614b9599c847190ef591e98761
glener10 <glenerpizzolato@gmail.com>
2025-12-10 10:04:46 -0300

    refactor: remove deprecated codes

--- src/utils/dirs.py
+++ src/utils/dirs.py
@@ -2,7 +2,7 @@ import os
 
 
 def remove_empty_dirs(root_dir):
-    for dirpath, dirnames, filenames in os.walk(root_dir, topdown=False):
+    for dirpath, dirnames, _ in os.walk(root_dir, topdown=False):
         for dirname in dirnames:
             full_path = os.path.join(dirpath, dirname)
             if not os.listdir(full_path):
