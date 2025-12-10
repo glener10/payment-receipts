594effba04a4a961e6f3c7d749219e625217cf60
glener10 <glenerpizzolato@gmail.com>
2025-12-09 22:38:32 -0300

    feat: improving mime_type util

--- src/utils/mime_type.py
+++ src/utils/mime_type.py
@@ -2,12 +2,17 @@ import os
 
 
 def get_mime_type(input):
-    _, suffix = os.path.splitext(input)
-    suffix = suffix.lower()
+    if input.startswith("."):
+        suffix = input.lower()
+    else:
+        _, suffix = os.path.splitext(input)
+        suffix = suffix.lower()
+
     if suffix == ".png":
         return "image/png"
     elif suffix in [".jpg", ".jpeg"]:
         return "image/jpeg"
     elif suffix == ".pdf":
         return "application/pdf"
-    raise None
+
+    raise ValueError(f"Unsupported file type: {suffix}")
